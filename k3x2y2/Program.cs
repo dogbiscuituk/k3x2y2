27e37d7d5568fe64385917ab6fa1acc7226287ed
John Kerr <John.Kerr@edinst.com>
2020-01-27 14:56:22 +0000

    Update Program.cs

--- k3x2y2/Program.cs
+++ k3x2y2/Program.cs
@@ -1,6 +1,8 @@
 ﻿namespace k3x2y2
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Text;
 
     class Program
@@ -13,17 +15,46 @@
         private static void Test()
         {
             Console.OutputEncoding = Encoding.UTF8;
-            var output = new StringBuilder();
-            for (var n = 0UL; n <= 10; n++)
+            for (var n = 2UL; n <= 1625; n++)
             {
-                var factors = Primes.Factorize(n);
+                var factors = Primes.Factorize(n).Select(f => (f.Item1, f.Item2));
                 var k = n * n;
-                output.Clear();
-                output.Append($"n = {n} = {factors.AsString()}; k = n² = {k}.");
-                Console.WriteLine(output);
+                checked
+                {
+                    Console.WriteLine($"n = {n} = {factors.AsString()}, k = n² = {k}, k³ = {k * k * k}:");
+                }
+                Console.WriteLine();
+                Distribute(1, 1, factors);
+                Console.WriteLine();
             }
             Console.WriteLine("Press the 'Any' key to continue...");
             Console.ReadKey();
+        }
+
+        private static void Distribute(ulong x, ulong y, IEnumerable<(ulong, int)> factors)
+        {
+            if (factors.Any())
+            {
+                var term = factors.First();
+                var prime = term.Item1;
+                var power = 3 * term.Item2;
+                for (var i = 0; i < power; i++)
+                    x *= prime;
+                for (var i = 0; i <= power; i++)
+                {
+                    Distribute(x, y, factors.Skip(1));
+                    if (i < power)
+                    {
+                        x /= prime;
+                        y *= prime;
+                    }
+                }
+            }
+            else
+                checked
+                {
+                    Console.WriteLine($"  x = {x}, y = {y}, x²y² = {x * x * y * y}");
+                }
         }
     }
 }
